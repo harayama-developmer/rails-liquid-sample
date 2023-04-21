a7bb9b37b784247a3e5e2c82e8d0fb7b49baf2ea
TakuyaHarayama <harayama.develop@gmail.com>
2023-04-22 01:21:43 +0900

    Add notify functions

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,11 @@
 class Message < ApplicationRecord
   validates :title, :body, presence: true
+
+  after_create_commit :notify_users
+
+  def notify_users
+    User.all.each do |user|
+      user.notifications.create!(title: title, body: body)
+    end
+  end
 end
